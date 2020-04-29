f9170c50ec36035fb849b197dc3a8b5758bf4ab1
Lukas Lazarek <LukasALazarek@gmail.com>
2020-04-29 11:16:15 -0500

    Add shell script for installing racket and running setup

---
+++ util/install-racket-and-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case "$1" in
+    ""|"-h"|"--help"|"h"|"help" )
+	echo "First argument must be project root"
+	exit 1
+	;;
+    * )
+	;;
+esac
+
+pushd "$1"
+
+INSTALLER="racket-7.6-x86_64-linux-cs.sh"
+wget "https://mirror.racket-lang.org/installers/7.6/$INSTALLER"
+chmod u+x ./$INSTALLER
+./$INSTALLER
+
+git clone https://github.com/LLazarek/rscript.git
+./racket/bin/raco pkg install ./rscript
+
+./racket/bin/racket blame-evaluation-gt/util/setup.rkt
+
+popd
