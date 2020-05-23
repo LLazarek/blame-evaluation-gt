ba7abed25d8d9f9f021564735dc0a8b5f2143d25
Lukas Lazarek <LukasALazarek@gmail.com>
2020-05-23 17:31:24 -0500

    Revert "Improve new expression-selector with simpler approach"
    This reverts commit 69e4d8512faf3025e128e731e7becd43b2557ec2.
    Without explicit cooperation from the mutation operators, operators can and do
    mess with the annotation locations

--- mutate/expression-selectors.rkt
+++ mutate/expression-selectors.rkt
@@ -74,14 +74,10 @@
      (define e-1-count (length (attribute e-1)))
      (define e-i-counts (map length (attribute e-i)))
      (list
-      #'(e-1 ... {~@ <ANN> e-i ...} ...)
+      #'([e-1 ...] [e-i ...] ...)
       (λ (mutated-stx)
         (syntax-parse mutated-stx
-          #:datum-literals [<ANN>]
-          [({~and mutated-e-1 {~not <ANN>}}
-            ...
-            {~seq <ANN> {~and mutated-e-i {~not <ANN>}} ...}
-            ...)
+          [([mutated-e-1 ...] [mutated-e-i ...] ...)
            (syntax/loc expr
              (mutated-e-1 ... {~@ annot.annotation-parts ... mutated-e-i ...} ...))])))]
     ;; ll: this is a bit naive, see tests below for #''(: a b c)
@@ -144,21 +140,21 @@
                    #'(f a b 42 c))
     (test-selector select-exprs-as-if-untyped
                    #'[a : Natural 42]
-                   #'[a <ANN> 42])
+                   #'[[a] [42]])
     (test-selector select-exprs-as-if-untyped
                    #'(λ ([x : T]) : R (+ 2 2))
-                   #'(λ ([x : T]) <ANN> (+ 2 2)))
+                   #'([λ ([x : T])] [(+ 2 2)]))
     (test-selector select-exprs-as-if-untyped
                    #'(for : T ([v : Boolean (in-list bools)])
                           (displayln v))
-                   #'(for <ANN> ([v : Boolean (in-list bools)])
-                          (displayln v)))
+                   #'([for] [([v : Boolean (in-list bools)])
+                             (displayln v)]))
     (test-selector select-exprs-as-if-untyped
                    #'(define (f [x : T]) : R (+ x 2))
-                   #'(define (f [x : T]) <ANN> (+ x 2)))
+                   #'([define (f [x : T])] [(+ x 2)]))
     (test-selector select-exprs-as-if-untyped
                    #'(define (f [x : T]) (: y R) (+ x 2))
-                   #'(define (f [x : T]) <ANN> (+ x 2)))
+                   #'([define (f [x : T])] [(+ x 2)]))
     (test-selector select-exprs-as-if-untyped
                    #'(define x ':)
                    #'(define x ':))
@@ -176,7 +172,7 @@
     ;; it's fine.
     (test-selector select-exprs-as-if-untyped
                    #''(: a b c)
-                   #'(quote <ANN>)))
+                   #'((quote) ())))
 
   (test-begin
     #:name select-exprs-as-if-untyped/reconstructor
