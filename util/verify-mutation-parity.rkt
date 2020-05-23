0308dadb665ecd5070f02c8660af8c18605fff0e
Lukas Lazarek <LukasALazarek@gmail.com>
2020-05-23 17:34:58 -0500

    verify-mutation-parity: find the lowest divergence or max index

--- util/verify-mutation-parity.rkt
+++ util/verify-mutation-parity.rkt
@@ -14,23 +14,46 @@
 
 (module common rscript
   (provide INDEX-SEARCH-RANGE
-           greatest-lower-bound-binary-search
+           lowest-upper-bound-binary-search
            (struct-out result)
-           (struct-out go-lower))
+           (struct-out try-go-lower)
+           (struct-out go-lower)
+           (struct-out go-higher)
+           (struct-out exhausted))
 
   (define INDEX-SEARCH-RANGE 20000)
 
   (struct result (index value) #:prefab)
+  (struct try-go-lower (value) #:prefab)
+  ;; (struct try-go-higher (value) #:prefab)
   (struct go-lower () #:prefab)
-  ;; index-result := (integer? -> (or/c '< '> any/c))
-  (define ((greatest-lower-bound-binary-search index-result)
+  (struct go-higher () #:prefab)
+  (struct exhausted () #:prefab)
+  ;; Finds the lowest index for which `index-result` returns `try-go-lower?`
+  ;; between `base-min` and `base-max`. For such an index where `index-result`
+  ;; returns `(try-go-lower v)`, this function returns `(result index v)`.
+  ;;
+  ;; Edge cases:
+  ;; If no such index can be found between `min` and `max` and `increase-max?`
+  ;; is #f, returns `(result i (exhausted))`, where `i` is the highest index
+  ;; for which `index-result` returns `go-higher?`.
+  ;;
+  ;; If `index-result` only returns `go-lower?`, the resulting index will
+  ;; instead be `(sub1 min)`.
+  ;;
+  ;; If `increase-max?` is #t and all results of `index-result` in `[min, max]`
+  ;; are `go-higher?`, then the search is repeated on `[max, max+max]`.
+  ;; And again if the same happens in that range.
+  ;; If `index-result` never returns anything other than `go-higher?` on
+  ;; `[0, +inf.0]` then this never terminates.
+  (define ((lowest-upper-bound-binary-search index-result)
            #:min [base-min 0]
            #:max [base-max INDEX-SEARCH-RANGE]
-           #:default-value [default-end-value (result #f #f)]
+           #:default-value [default-end-value (exhausted)]
            #:increase-max? [increase-max? #f])
     (let search ([min base-min]
                  [max base-max]
-                 [lowest-end-value-found default-end-value])
+                 [lowest-end-value-found (result +inf.0 default-end-value)])
       (cond [(and (> min max)
                   increase-max?
                   (zero? (modulo max base-max)))
@@ -38,40 +61,99 @@
                      (+ max base-max)
                      lowest-end-value-found)]
             [(> min max)
-             lowest-end-value-found]
+             (match lowest-end-value-found
+               [(result +inf.0 v)
+                (result max v)]
+               [else lowest-end-value-found])]
             [else
              (define index-to-try (+ min (quotient (- max min) 2)))
+             (define lowest-index (result-index lowest-end-value-found))
              (match (index-result index-to-try)
                [(go-lower)
                 (search min
                         (sub1 index-to-try)
                         lowest-end-value-found)]
-               [value
+               [(go-higher)
                 (search (add1 index-to-try)
                         max
-                        (result index-to-try
-                                value))])])))
+                        lowest-end-value-found)]
+               [(try-go-lower value)
+                (search min
+                        (sub1 index-to-try)
+                        (if (< index-to-try lowest-index)
+                            (result index-to-try value)
+                            lowest-end-value-found))]
+               #;[(try-go-higher value)
+                (search (add1 index-to-try)
+                        max
+                        (if (< index-to-try lowest-index)
+                            (result index-to-try value)
+                            lowest-end-value-found))])])))
 
   (module+ test
     (require ruinit)
     (test-begin
-      #:name greatest-lower-bound-binary-search
-      (ignore (define search (greatest-lower-bound-binary-search
-                              (match-lambda
-                                [(? (<=/c 50) n) (- n)]
-                                [else (go-lower)]))))
+      #:name lowest-upper-bound-binary-search
+      (ignore (define search
+                (lowest-upper-bound-binary-search
+                 (match-lambda
+                   [(? (</c 40)) (go-higher)]
+                   [(? (and/c (>=/c 40) (<=/c 60)) n) (try-go-lower (- n))]
+                   [else (go-lower)]))))
       (test-match (search)
-                  (result 50 -50))
-      (test-match (search #:min 60)
-                  (result #f #f))
-      (test-match (search #:max 30)
-                  (result 30 -30))
+                  (result 40 -40))
+      (test-match (search #:min 70)
+                  (result 69 (exhausted)))
+      (test-match (search #:min 70
+                          #:default-value 'foobar)
+                  (result 69 'foobar))
+      (test-match (search #:max 30
+                          #:increase-max? #f)
+                  (result 30 (exhausted)))
       (test-match (search #:max 30
                           #:increase-max? #t)
-                  (result 50 -50))
-      (test-match (search #:min 60
-                          #:default-value 'foobar)
-                  'foobar))))
+                  (result 40 -40))
+
+      ;; Simulate a program that has no mutants!
+      (ignore (define search (lowest-upper-bound-binary-search
+                              (match-lambda [else (go-lower)]))))
+      (test-match (search)
+                  (result -1 (exhausted)))
+
+      ;; The inverse...
+      (ignore (define search (lowest-upper-bound-binary-search
+                              (match-lambda [else (go-higher)]))))
+      (test-match (search #:max 100)
+                  (result 100 (exhausted)))
+
+
+      ;; Simulate a program that passes on all mutants, and has 50 mutants. Then
+      ;; we want index 49, and the result should be `exhausted?`.
+      (ignore (define search (lowest-upper-bound-binary-search
+                              (match-lambda
+                                [(? (</c 50)) (go-higher)]
+                                [else (go-lower)]))))
+      (test-match (search)
+                  (result 49 (exhausted)))
+
+      ;; Simulate a program that diverges on the first mutant, and only have 50
+      ;; mutants. Then we want index 0.
+      (ignore (define search (lowest-upper-bound-binary-search
+                              (match-lambda
+                                [(? (>/c 50)) (go-lower)]
+                                [i (try-go-lower (~a i))]))))
+      (test-match (search)
+                  (result 0 "0"))
+
+      ;; Simulate a program that diverges on the third mutant up to the tenth,
+      ;; then runs out mutants. Then we want index 2.
+      (ignore (define search (lowest-upper-bound-binary-search
+                              (match-lambda
+                                [(? (>/c 9)) (go-lower)]
+                                [(? (between/c 2 9) i) (try-go-lower (~a i))]
+                                [(? (</c 2)) (go-higher)]))))
+      (test-match (search)
+                  (result 2 "2")))))
 
 (module parity-check rscript
   (provide (struct-out no-more-mutants)
@@ -80,12 +162,38 @@
 
   (require "../configurations/configure-benchmark.rkt"
            "../mutate/mutate-program.rkt"
+           "../mutate/logger.rkt"
            "../runner/mutation-runner.rkt"
            "../runner/program.rkt"
            "../util/path-utils.rkt"
-           (submod ".." common))
+           (submod ".." common)
+           racket/logging)
 
   (struct no-more-mutants () #:prefab)
+  (define (extract-mutation stx index)
+    (define-values {read-end write-end} (make-pipe))
+    (define mutated-id
+      (with-logging-to-port write-end
+        (thunk
+         (define-values {_ id}
+           (mutate-module stx index))
+         id)
+        #:logger mutate-logger
+        'info
+        'mutate))
+    (define mutation
+      (match (regexp-match #px"(?m:mutate: Mutating .*)"
+                           read-end)
+        [(list str) str]))
+    (list mutated-id
+          (strip-paths mutation)))
+
+  (define (strip-paths str)
+    (define stx-path-rx @~a{#<syntax:.+\.rkt:\d+:\d+})
+    (regexp-replace* (pregexp @~a{@stx-path-rx (.+)> -> @stx-path-rx (.+)>})
+                     str
+                     @~a{\1 -> \2}))
+
   (define/contract (mutated-id-for-typed/untyped-agrees? a-benchmark
                                                          a-module-to-mutate
                                                          mutation-index)
@@ -94,8 +202,10 @@
      natural?
      . -> .
      (or/c no-more-mutants?
-           (list/c (or/c no-more-mutants? symbol?)
-                   (or/c no-more-mutants? symbol?))
+           (list/c (or/c no-more-mutants?
+                         (list/c symbol? any/c))
+                   (or/c no-more-mutants?
+                         (list/c symbol? any/c)))
            #t))
     (define mutateds
       (for/list ([typed-or-untyped-files (in-list (list benchmark-typed
@@ -104,14 +214,14 @@
                                             a-module-to-mutate))
         (define mod (make-mod mod-path))
         (with-handlers ([mutation-index-exception? (const (no-more-mutants))])
-          (define-values {_ id}
-            (mutate-module (mod-stx mod) mutation-index))
-          id)))
+          (extract-mutation (mod-stx mod) mutation-index))))
     (match mutateds
       [(list (no-more-mutants) (no-more-mutants))
        (no-more-mutants)]
-      [(list (? symbol? a)     (? symbol? b))
-       #:when (equal? a b)
+      [(list (list (? symbol? a) e-a)
+             (list (? symbol? b) e-b))
+       #:when (and (equal? a b)
+                   (equal? e-a e-b))
        #t]
       [(list a b) (list a b)]))
 
@@ -122,9 +232,9 @@
                                                    the-module-to-mutate
                                                    index)
         [(no-more-mutants) (go-lower)]
-        [#t #t]
-        [(list a b) (list a b)]))
-    (define search (greatest-lower-bound-binary-search result-for-index))
+        [#t                (go-higher)]
+        [(list a b)        (try-go-lower (list a b))]))
+    (define search (lowest-upper-bound-binary-search result-for-index))
     (search))
 
   (define (process-parity-check-main)
@@ -190,7 +300,7 @@
    @~a{@benchmark-name "@mod-to-mutate" complete in @time-elapsed-mins min})
   (define parity-worker-result
     (match mutated-id-results
-      [(result index #t)
+      [(result index (exhausted))
        (log-parity-info @~a{@complete-msg : ok, max index @index})
        (done 'ok)]
       [(result index (list typed-mutated untyped-mutated))
@@ -198,13 +308,12 @@
        (done @~a{
                  Typed/untyped disagree in @benchmark-name @;
                  module @mod-to-mutate starting at mutation index @index
-                 Typed mutates:   @typed-mutated
-                 Untyped mutates: @untyped-mutated
+                 Typed mutates:   @(first typed-mutated)
+                 Mutation:        @(second typed-mutated)
+                 Untyped mutates: @(first untyped-mutated)
+                 Mutation:        @(second untyped-mutated)
 
                  })]
-      [(result #f #f)
-       (log-parity-info @~a{@complete-msg : has no mutants})
-       (done 'ok)]
       [(result index anything-else)
        (raise-user-error
         'verify-mutation-parity
