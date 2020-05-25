25ed8890e8963e1e125474fa274084d532ba8856
Lukas Lazarek <LukasALazarek@gmail.com>
2020-05-25 16:15:30 -0500

    Fix for/first*

--- util/for-first-star.rkt
+++ util/for-first-star.rkt
@@ -21,9 +21,18 @@
     (define-simple-macro (for*?/first*? (clause (... ...)) body (... ...))
       (for*?/first (clause
                     (... ...)
+                    #:when #t
                     [value (in-value (let () body (... ...)))]
                     #:when value)
                    value))
     ...))
 
 (define-for/first*s for/first* for*/first*)
+
+(module+ test
+  (require ruinit)
+  (test-begin
+    #:name for/first*
+    (test-equal? (for/first* ([i (in-range 5)])
+                             (if (< i 3) #f (* i 2)))
+                 6)))
