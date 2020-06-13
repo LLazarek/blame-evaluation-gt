24426fe23a1086aac7e8fdf85937881fff177aa5
Lukas Lazarek <LukasALazarek@gmail.com>
2020-06-13 15:56:26 -0500

    Add configuration system to plug in configurable components

---
+++ configurables/all-configurables.rktd
@@ -0,0 +1,4 @@
+#hash(("mutation" . #hash(("type-mistakes-in-code" . "mutation/mutate-benchmark.rkt")))
+      ("mutant-sampling" . #hash(("none" . "mutant-sampling/no-sampling.rkt")
+                                 ("within-mutators" . "mutant-sampling/within-mutators.rkt")))
+      ("blame-following" . #hash(("first" . "blame-following/first.rkt"))))
---
+++ configurables/configurables.rkt
@@ -0,0 +1,48 @@
+#lang at-exp racket
+
+(require "../util/optional-contracts.rkt")
+(provide (contract-out
+          [load-specific (string? string? symbol? . -> . any/c)]
+          [load-configured (path-string? string? symbol? . -> . any/c)]))
+
+(require racket/runtime-path)
+
+(define-runtime-path configurables-path "all-configurables.rktd")
+
+(unless (file-exists? configurables-path)
+  (raise-user-error 'configurables
+                    @~a{Unable to find configuration at @configurables-path}))
+
+(define configurables (file->value configurables-path))
+
+(unless (hash? configurables)
+  (raise-user-error 'configurables
+                    @~a{Malformed configurables at @configurables}))
+
+(define (read-implementation-for category name)
+  (hash-ref (hash-ref configurables
+                      category
+                      (λ _ (error
+                            'read-implementation-for
+                            @~a{@category not found in configurables})))
+            name
+            (λ _ (error
+                  'read-implementation-for
+                  @~a{@name not found in configurables for @category}))))
+
+(define (load-specific category name id)
+  (define implementation (read-implementation-for category name))
+  (dynamic-require implementation
+                   id
+                   (λ _
+                     (error
+                      'load-specific
+                      @~a{
+                          Implementation from configurables @implementation @;
+                          either does not exist or does not provide @id
+                          }))))
+
+(define (load-configured config-path category id)
+  (define config (file->value config-path))
+  (define name (hash-ref config category))
+  (load-specific category name id))
