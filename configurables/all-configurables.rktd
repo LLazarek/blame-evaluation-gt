1f3dc43a2d4635da2293fd919cb53a7bc61d525f
Lukas Lazarek <LukasALazarek@gmail.com>
2020-06-13 16:23:14 -0500

    Make configurable implementation paths work regardless of location

--- configurables/all-configurables.rktd
+++ configurables/all-configurables.rktd
@@ -1,3 +1,4 @@
+;; Paths to implementations are relative to this file
 #hash(("mutation" . #hash(("type-mistakes-in-code" . "mutation/mutate-benchmark.rkt")))
       ("mutant-sampling" . #hash(("none" . "mutant-sampling/no-sampling.rkt")
                                  ("within-mutators" . "mutant-sampling/within-mutators.rkt")))
--- configurables/configurables.rkt
+++ configurables/configurables.rkt
@@ -8,6 +8,9 @@
 (require racket/runtime-path)
 
 (define-runtime-path configurables-path "all-configurables.rktd")
+(define implementation-paths-root
+  (let-values ([{parent _1 _2} (split-path configurables-path)])
+    parent))
 
 (unless (file-exists? configurables-path)
   (raise-user-error 'configurables
@@ -31,16 +34,19 @@
                   @~a{@name not found in configurables for @category}))))
 
 (define (load-specific category name id)
-  (define implementation (read-implementation-for category name))
-  (dynamic-require implementation
-                   id
-                   (λ _
-                     (error
-                      'load-specific
-                      @~a{
-                          Implementation from configurables @implementation @;
-                          either does not exist or does not provide @id
-                          }))))
+  (define implementation-rel-path (read-implementation-for category name))
+  (define implementation-abs-path (build-path implementation-paths-root
+                                              implementation-rel-path))
+  (dynamic-require
+   `(file ,(path->string implementation-abs-path))
+   id
+   (λ _
+     (error
+      'load-specific
+      @~a{
+          Implementation from configurables @implementation-rel-path @;
+          either does not exist or does not provide @id
+          }))))
 
 (define (load-configured config-path category id)
   (define config (file->value config-path))
