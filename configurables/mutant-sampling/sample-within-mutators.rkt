20a82070c6e22f258161f9abe06cfed911692d67
Lukas Lazarek <LukasALazarek@gmail.com>
2020-06-14 18:15:05 -0500

    Handle not sampling any mutants in a module

--- configurables/mutant-sampling/sample-within-mutators.rkt
+++ configurables/mutant-sampling/sample-within-mutators.rkt
@@ -7,9 +7,7 @@
 (require "mutant-selector.rkt"
          (prefix-in db: "../../db/db.rkt")
          "../../configurations/configure-benchmark.rkt"
-         "../../util/mutant-util.rkt"
-         racket/runtime-path
-         racket/format)
+         racket/runtime-path)
 
 (define-runtime-path default-samples-db "samples/default.rktdb")
 (define mutation-analysis-samples-db (make-parameter default-samples-db))
@@ -20,21 +18,10 @@
 
 ;; benchmark/c module-name? -> summary?
 (define (samples-for the-bench module-to-mutate-name)
-  (define (empty-if-no-mutants)
-    (define max-index (max-mutation-index module-to-mutate-name
-                                          the-bench))
-    (if (< max-index 0)
-        '()
-        (error
-         'sample-within-mutators
-         @~a{
-             No samples recorded in db @(mutation-analysis-samples-db) @;
-             for module @~e[module-to-mutate-name]
-             })))
   (define bench-name (benchmark->name the-bench))
   (define db (db:get (mutation-analysis-samples-db)))
   (define samples-by-module (db:read db bench-name))
   (hash-ref samples-by-module
             module-to-mutate-name
-            empty-if-no-mutants))
+            '()))
 
