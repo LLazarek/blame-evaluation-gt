d00e0386e1c8357c2f912d68113c2f7c326292bf
Lukas Lazarek <LukasALazarek@gmail.com>
2020-10-12 10:26:56 -0500

    info.rkt: omit all data analysis scripts from raco test

--- info.rkt
+++ info.rkt
@@ -2,4 +2,4 @@
 
 (define test-omit-paths '("util/setup.rkt"
                           "mutation-analysis/plot-mutation-analyses.rkt"
-                          "data-analysis/bt-length-distribution.rkt"))
+                          "data-analysis"))
