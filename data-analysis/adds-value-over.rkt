73f23374e850d81a213100be56a59238af159004
Lukas Lazarek <LukasALazarek@gmail.com>
2021-02-26 22:28:17 -0600

    figures: Fix bug with memoizing on disk

--- data-analysis/adds-value-over.rkt
+++ data-analysis/adds-value-over.rkt
@@ -26,7 +26,7 @@
 
 (define (simple-memoize f #:on-disk [cache-path #f])
   (let ([cache (if (and cache-path (file-exists? cache-path))
-                   (file->value cache-path)
+                   (make-hash (hash->list (file->value cache-path)))
                    (make-hash))])
     (λ args
       (cond [(hash-ref cache args #f) => values]
