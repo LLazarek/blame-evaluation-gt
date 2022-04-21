f07b2b1702312e5d07ea82de91a32b24a6569448
Lukas Lazarek <LukasALazarek@gmail.com>
2022-04-21 16:30:28 -0500

    Add sketch of remaining experiment mode configs, with some todos

--- configurables/configs/TR.rkt
+++ configurables/configs/TR.rkt
@@ -11,7 +11,6 @@
   (configure! mutant-sampling          none)
   (configure! mutant-filtering         select-type/runtime/ctc-erroring-max-config-mutants)
   (configure! module-selection-for-mutation interface-module-only)
-  (configure! module-instrumentation   none)
   (configure! benchmark-runner         run-it)
   (configure! blame-following          pick-some
               ; runtime-error-with-blame
@@ -24,4 +23,5 @@
               config-has-both-sides-of-interface-untyped?)
   (configure! trail-completion         any-type-error/blamed-at-max)
 
+  (configure! module-instrumentation   none)
   (configure! program-instrumentation  instrument-modules-and-insert-interface-adapter-module))
--- configurables/configs/transient-oldest.rkt
+++ configurables/configs/transient-oldest.rkt
@@ -1,14 +1,16 @@
 #lang racket/base
 
 (require "../configurables.rkt"
-         "blame-following-common.rkt")
+         "blame-following-common.rkt"
+         "bt-root-sampling-common.rkt")
 
 (provide install!)
 
 (define (install!)
-  (configure! mutation                 type-mistakes-in-code)
-  (configure! mutant-sampling          use-pre-selected-samples "../dbs/code-mutations/mutant-samples.rktdb")
-  (configure! module-instrumentation   transient-types "../dbs/code-mutations/transient-special-cases.rktdb")
+  (configure! mutation                 type-interface-mistakes)
+  (configure! mutant-sampling          none)
+  (configure! mutant-filtering         select-type/runtime/ctc-erroring-max-config-mutants)
+  (configure! module-selection-for-mutation interface-module-only)
   (configure! benchmark-runner         run-it)
   (configure! blame-following          pick-some
               ; runtime-error-with-blame
@@ -17,5 +19,9 @@
               select-top-of-context/filter-typed
               ; blame
               select-last-blamed-pair)
+  ;; lltodo
   (configure! bt-root-sampling         pre-selected "../dbs/code-mutations/pre-selected-bt-roots.rktdb")
-  (configure! trail-completion         any-type-error/blamed-at-max))
+  (configure! trail-completion         any-type-error/blamed-at-max)
+
+  (configure! module-instrumentation   transient-types "../dbs/code-mutations/transient-special-cases.rktdb")
+  (configure! program-instrumentation  instrument-modules-and-insert-interface-adapter-module))
