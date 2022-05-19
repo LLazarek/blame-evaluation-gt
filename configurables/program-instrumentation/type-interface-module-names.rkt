efe218d22bce80ad2c60b850d9ac93736fda0083
Lukas Lazarek <LukasALazarek@gmail.com>
2022-05-19 15:11:57 -0500

    Oops, add forgotten module for last commit

---
+++ configurables/program-instrumentation/type-interface-module-names.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(provide type-interface-file-name
+         type-interface-file-rename)
+
+(define type-interface-file-name "type-interface.rkt")
+(define type-interface-file-rename "original-type-interface.rkt")
