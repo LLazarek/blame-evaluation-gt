8f5a6d44639bef6834c69fa36a3a85b3105cbed5
Lukas Lazarek <LukasALazarek@gmail.com>
2022-11-22 12:14:45 -0600

    type-with-transient: redirect r/t/c requires to the custom transient version
    The custom version is added in gtp-benchmarks commit 10358ab

--- configurables/module-instrumentation/type-with-transient.rkt
+++ configurables/module-instrumentation/type-with-transient.rkt
@@ -30,21 +30,51 @@
                           lang}
          (mod-begin . body))
        #:with t/r/s (datum->syntax #'lang 'typed/racket/shallow)
+       #:with instrumented-body (redirect-require/typed/checks #'body)
        (syntax/loc this-syntax
-         (module name t/r/s (mod-begin . body)))]
+         (module name t/r/s (mod-begin . instrumented-body)))]
       [other this-syntax]))
   (struct-copy mod a-mod
                [stx instrumented-stx]))
+
+(define-syntax-class tlf
+  #:description "top level form"
+  (pattern ({~datum require} {~alt "../../../utilities/require-typed-check-provide.rkt"
+                                   {~datum require-typed-check}
+                                   other-reqs}
+                             ...)
+           #:with redirected (datum->syntax this-syntax
+                                            `(require
+                                              "../../../utilities/require-typed-check-provide-transient.rkt"
+                                              . ,(attribute other-reqs))))
+  (pattern something-else
+           #:with redirected this-syntax))
+
+(define (redirect-require/typed/checks stx)
+  (syntax-parse stx
+    [[top-level-form:tlf ...]
+     (syntax/loc this-syntax
+       (top-level-form.redirected ...))]))
 
 (define (mod-stx/replace-special-cases a-mod)
   (define mod-relative-path
     (benchmark-mod-relative-path (mod-path a-mod)))
   (define resolved-db-path (build-path configurables
                                        (transient-special-cases-db)))
-  (define db (db:get resolved-db-path))
-  (match (db:read db
-                  mod-relative-path
-                  #f)
+  (define replacement-mod
+    (cond [(db:path-to-db? resolved-db-path)
+           (define db (db:get resolved-db-path))
+           (db:read db
+                    mod-relative-path
+                    #f)]
+          [else
+           (log-warning @~a{
+                            type-with-transient: @;
+                            transient-special-cases-db is misconfigured or not configured: @;
+                            @(transient-special-cases-db)
+                            })
+           #f]))
+  (match replacement-mod
     [#f (mod-stx a-mod)]
     [(? string? mod-str)
      (with-input-from-string mod-str
@@ -63,29 +93,43 @@
 (module+ test
   (require ruinit
            racket)
-  (when (db:path-to-db? default-transient-special-cases-db)
-    (test-begin
-      #:name instrument-module
-      (ignore (define db (db:get default-transient-special-cases-db)))
-      (string? (db:read db "lnm/typed/lnm-plot.rkt"))
+  (test-begin
+    #:name instrument-module
+    (test-equal?
+     (syntax->datum (mod-stx
+                     (instrument-module
+                      (mod "foo/typed/main.rkt"
+                           #'(module foo typed/racket (#%module-begin 42))))))
+     '(module foo typed/racket/shallow (#%module-begin 42)))
+    (test-equal?
+     (syntax->datum (mod-stx
+                     (instrument-module
+                      (mod "foo/untyped/main.rkt"
+                           #'(module foo racket (#%module-begin 42))))))
+     '(module foo racket (#%module-begin 42)))
 
-      (test-equal?
-       (syntax->datum (mod-stx
-                       (instrument-module
-                        (mod "foo/typed/main.rkt"
-                             #'(module foo typed/racket (#%module-begin 42))))))
-       '(module foo typed/racket/shallow (#%module-begin 42)))
-      (test-equal?
-       (syntax->datum (mod-stx
-                       (instrument-module
-                        (mod "foo/untyped/main.rkt"
-                             #'(module foo racket (#%module-begin 42))))))
-       '(module foo racket (#%module-begin 42)))
-
-      (test-match
-       (syntax->datum (mod-stx
-                       (instrument-module
-                        (mod "lnm/typed/lnm-plot.rkt"
-                             #'(module lnm-plot typed/racket (#%module-begin 42))))))
-       `(module ,_ typed/racket/shallow
-          (#%module-begin . ,_))))))
+    (test-match
+     (syntax->datum (mod-stx
+                     (instrument-module
+                      (mod "lnm/typed/lnm-plot.rkt"
+                           #'(module lnm-plot typed/racket (#%module-begin 42))))))
+     `(module ,_ typed/racket/shallow
+        (#%module-begin . ,_)))
+    (test-match
+     (syntax->datum (mod-stx
+                     (instrument-module
+                      (mod "foo/typed/main.rkt"
+                           #'(module foo typed/racket (#%module-begin
+                                                       (require require-typed-check
+                                                                other-thing
+                                                                "../../../utilities/require-typed-check-provide.rkt"
+                                                                foobar)
+                                                       (define (f x) x)
+                                                       42))))))
+     `(module ,_ typed/racket/shallow
+        (#%module-begin
+         (require "../../../utilities/require-typed-check-provide-transient.rkt"
+                  other-thing
+                  foobar)
+         (define (f x) x)
+         42)))))
