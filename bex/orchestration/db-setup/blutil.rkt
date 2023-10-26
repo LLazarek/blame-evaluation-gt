991b6467f1d6c47b0f3444afbadfd961003b5e43
Lukas Lazarek <LukasALazarek@gmail.com>
2023-10-26 15:37:26 -0500

    bex: two lingering edits to bring Akash's work up to date
    In particular wrt the latest db setup process.

--- bex/orchestration/db-setup/blutil.rkt
+++ bex/orchestration/db-setup/blutil.rkt
@@ -3,9 +3,9 @@
 (require "db-setup.rkt")
 
 (db-setup-script
- #:mutation-analysis-config "../../configurables/blutil-configs/mutation-type-error-analysis.rkt"
+ #:mutation-analysis-config "../../configurables/blutil-configs/mutation-code-mistake-analysis.rkt"
  #:analyze-type-mutation-categories? #f
- #:experiment-config-with-which-analyze-mutants-dynamic-errors "../../configurables/blutil-configs/dynamic-error-analysis.rkt"
+ #:experiment-config-with-which-analyze-mutants-dynamic-errors "../../configurables/blutil-configs/blame.rkt"
  #:dynamic-error-filtering-lattice-config-id 'bot
  #:dynamic-error-interestingness-filter? #t
  #:search-for-interesting-scenarios? #f
