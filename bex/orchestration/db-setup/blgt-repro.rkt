699387f99e3c92cc3e29d5bbb9f1b1f77bcab182
Lukas Lazarek <LukasALazarek@gmail.com>
2023-10-26 12:45:15 -0500

    bex/orchestration: refactor db setup to track flows in seperate files
    Before setup-all-dbs.rkt was the single *current* flow/process, and it had these
    flags to tweak it but for a given experiment we really only wanted a particular
    flow. So the process was just to edit and commit setup-all-dbs.rkt.
    
    That is getting real confusing now that we've got multiple versions of the
    experiment going at a time, and also made it pretty difficult to keep track of
    the flow used in a given experiment -- especially since the underlying programs
    changed over time, too!
    
    Now, every version of the experiment we run can have its own file defining the
    db setup process, so that we keep track of the differences and can keep them up
    to date with changes to the underlying programs as necessary.

---
+++ bex/orchestration/db-setup/blgt-repro.rkt
@@ -0,0 +1,14 @@
+#lang at-exp racket
+
+(require "db-setup.rkt")
+
+(db-setup-script
+ #:mutation-analysis-config "../../configurables/blgt-configs/mutation-type-error-analysis.rkt"
+ #:analyze-type-mutation-categories? #f
+ #:experiment-config-with-which-analyze-mutants-dynamic-errors "../../configurables/blgt-configs/dynamic-error-analysis-natural.rkt"
+ #:dynamic-error-filtering-lattice-config-id 'bot
+ #:dynamic-error-interestingness-filter? #f
+ #:search-for-interesting-scenarios? #t
+ #:mutants-to-sample-per-benchmark 80
+ #:pre-compute-config "../../configurables/blgt-configs/erasure-pre-compute-benchmark-results.rkt")
+
---
+++ bex/orchestration/db-setup/bltym.rkt
@@ -0,0 +1,14 @@
+#lang at-exp racket
+
+(require "db-setup.rkt")
+
+(db-setup-script
+ #:mutation-analysis-config "../../configurables/configs/mutation-type-error-analysis.rkt"
+ #:analyze-type-mutation-categories? #t
+ #:experiment-config-with-which-analyze-mutants-dynamic-errors "../../configurables/configs/dynamic-error-analysis-natural.rkt"
+ #:dynamic-error-filtering-lattice-config-id 'bot
+ #:dynamic-error-interestingness-filter? #f
+ #:search-for-interesting-scenarios? #t
+ #:mutants-to-sample-per-benchmark 'all
+ #:pre-compute-config "../../configurables/configs/erasure-pre-compute-benchmark-results.rkt")
+
